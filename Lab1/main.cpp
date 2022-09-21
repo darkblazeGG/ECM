124f4c995cc904c5c3b754cbfbba10bc56f8aca6
DarkBlaze <darkblaze.ru@yandex.ru>
2022-09-21 17:16:34 +0700

    init

---
+++ Lab1/main.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+double Result(int N, double x)
+{
+    double result = 0;
+    for (int i = 1; i < N; i++)
+        result += pow(-1, i + 1) * pow(x, i) / i;
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    double result, x = 0.1;
+    struct timespec start, end;
+    int N = strtol(argv[1], NULL, 10);
+
+    // clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+
+    result = Result(N, x);
+
+    // clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+
+    printf("Spent %lf sec.\n for get result %lf", end.tv_sec - start.tv_sec + 0.000000001 * (end.tv_nsec - start.tv_nsec), result);
+
+    return 0;
+}
